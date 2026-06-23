5c4326eeeeb1be45bcb802abb64623179b8457ad
yurii <abacaki98@gmail.com>
2026-06-22 23:23:46 -0300

    Pindorama_Arena

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+Console.WriteLine("Bem vindo a Arena de Pindorama! 🏟️");
+Console.Write("\nDigite o nome do seu guerreiro: ");
+string nomeGuerreiro = Console.ReadLine();
+
+Console.WriteLine("\nEscolha seu inimigo: \r\n\r\n1 - Lobisomem 🐺 \n2 - Mula Sem Cabeça 🐴🔥");
+string escolhaInimigo = Console.ReadLine();
+
+FolkFighter warrior = new FolkFighter(nomeGuerreiro, 110, 20);
+FolkFighter monster;
+
+if (escolhaInimigo == "1")
+{
+    monster = new FolkFighter("Lobisomem", 100, 18);
+}
+else if (escolhaInimigo == "2")
+{
+    monster = new FolkFighter("Mula Sem Cabeça", 130, 22);
+}
+else
+{
+    Console.WriteLine("Opção inválida! Lobisomem escolhido.");
+    monster = new FolkFighter("Lobisomem", 100, 18);
+}
+
+PindoramaArena arena = new PindoramaArena();
+
+arena.RunBattle(warrior, monster);
+arena.ShowLog();
+
+
+Console.WriteLine("\nFim da batalha!");
